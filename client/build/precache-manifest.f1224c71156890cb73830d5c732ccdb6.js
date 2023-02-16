54f1f3ef9085dce9a0d09178a0b3b805020fe3ac
blahablast <blahasam@gmail.com>
2023-02-16 12:17:38 -0500

    updated the heroku for hosting

---
+++ client/build/precache-manifest.f1224c71156890cb73830d5c732ccdb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a6abababaaa478a94c1e08cf70ab11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbe77b5161cdbd143969",
+    "url": "/static/css/main.705d46d5.chunk.css"
+  },
+  {
+    "revision": "994d8fec124846c67b4a",
+    "url": "/static/js/2.cfb6dca8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.cfb6dca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbe77b5161cdbd143969",
+    "url": "/static/js/main.c956ce2e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
